62e57f294c20d2b543eee1fe519ead48ec08d034
Daniel Flores <Daniel.Flores03@student.csulb.edu>
2019-05-01 22:17:08 -0700

    Update and rename database.sql to CreateTables.sql

--- src/CreateTables.sql
+++ src/CreateTables.sql
@@ -296,20 +296,3 @@ CREATE TABLE Shift (
   CONSTRAINT Shift_pk PRIMARY KEY (ShiftDate, EID),
   CONSTRAINT Shift_fk FOREIGN KEY (EID) REFERENCES Employee (EID)
 );
-
---data for customers
-INSERT INTO Customer (CID, CName, Email) VALUES
-  (002, 'Bill Fairbanks', 'b.fairbanks@sis.gov.uk'),
-  (003, 'Jason Walters', 'j.walters@sis.gov.uk'),
-  (004, 'Aidan Flemmings', 'a.flemmings@sis.gov.uk'),
-  (005, 'Stuart Thomas', 's.thomas@sis.gov.uk'),
-  (006, 'Alec Trevelyan', 'a.trevelyan@janus.org')
-  (007, 'James Bond', 'j.bond@sis.gov.uk');
-
---data for Hall of Hall Of Fame
-INSERT INTO HallOfFame (CID, InductionDate, itemName, photograph) VALUES
-  (007, '1995-11-17', 'Chicken Chow Mein', 1),
-  (006, '1997-08-25', 'Beef Egg Goo Young', 0);
-
---data for Addresses
-INSERT INTO Addresses (CID, AddressID, AddressType, Street, City, State, ZipCode) VALUES
