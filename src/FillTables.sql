4c71ebd8dc765b9b1d5b039fa0b3c99cb0a3b2a9
RickJames7 <48032896+RickJames7@users.noreply.github.com>
2019-05-02 14:31:56 -0700

    Update FillTables.sql
    Add employees and quotations

--- src/FillTables.sql
+++ src/FillTables.sql
@@ -14,17 +14,55 @@ INSERT INTO HallOfFame (CID, InductionDate, itemName, photograph) VALUES
 
 --data for employees
 INSERT INTO Employee (EID, name, jobTitle, DOB, shift) VALUES
-(001, Aarron Rodgers, HeadChef, 1988-03-12, AM),
-(002, Tom Brady, HeadChef, 1975-02-16, PM),
-(003, Jared Goff, SousChef, 1994-06-06, AM),
-(004, Phillip Rivers, SousChef, 1980-03-25, AM),
-(005, Jimmy Garropolo, SousChef, 1993-11-11, PM),
-(006, Patrick Mahomes, SousChef, 1995-05-11, PM),
-(007, Alex Smith, LineCook, 1987-07-13, AM),
-(008, Dereck Carr, LineCook, 1991-03-08, AM),
-(009, Kyler Murray, LineCook, 1997-01-28, AM),
-(010, Joe Montana, LineCook, 1965-11-01, AM),
-(011, Joe Theisman, LineCook, 1963-02-02, AM),
-(012, Baker Mayfield, LineCook, 1996-12-11, PM),
-(013, Drew Brees, LineCook, 1978-01-03, PM),
-(0, Drew Brees, LineCook, 1978-01-03, PM),
+(001, 'Aarron Rodgers', 'HeadChef', 1988-03-12, 'AM'),
+(002, 'Tom Brady', 'HeadChef', 1975-02-16, 'PM'),
+(003, 'Jared Goff', 'SousChef', 1994-06-06, 'AM'),
+(004, 'Phillip Rivers', 'SousChef', 1980-03-25, 'AM'),
+(005, 'Jimmy Garropolo', 'SousChef', 1993-11-11, 'PM'),
+(006, 'Patrick Mahomes', 'SousChef', 1995-05-11, 'PM'),
+(007, 'Alex Smith', 'LineCook', 1987-07-13, 'AM'),
+(008, 'Dereck Carr', 'LineCook', 1991-03-08, 'AM'),
+(009, 'Kyler Murray', 'LineCook', 1997-01-28, 'AM'),
+(010, 'Joe Montana', 'LineCook', 1965-11-01, 'AM'),
+(011, 'Joe Theisman', 'LineCook', 1963-02-02, 'AM'),
+(012, 'Baker Mayfield', 'LineCook', 1996-12-11, 'PM'),
+(013, 'Drew Brees', 'LineCook', 1978-01-03, 'PM'),
+(014, 'Andrew Luck', 'LineCook', 1988-05-27, 'PM'),
+(015, 'Russell Wilson', 'LineCook', 1985-02-06, 'PM'),
+(016, 'Matt Ryan', 'LineCook', 1987-04-04, 'PM'),
+(017, 'Taylor Swift', 'DishWasher', 1985-02-09, 'AM'),
+(018, 'Sara Jones', 'DishWasher', 1992-03-12, 'AM'),
+(019, 'Emily Watson', 'DishWasher', 1990-05-05, 'AM'),
+(020, 'Jacob Little', 'DishWasher', 1995-07-16, 'AM'),
+(021, 'Alex Jones', 'DishWasher', 1983-12-12, 'AM'),
+(022, 'Tre Lyles', 'DishWasher', 1989-06-09, 'PM'),
+(023, 'Dantey Wilder', 'DishWasher', 1987-12-03, 'PM'),
+(024, 'Rebecca Smits', 'DishWasher', 1985-02-09, 'PM'),
+(025, 'Jeanie Busse', 'DishWasher', 1978-05-11, 'PM'),
+(026, 'Jackson Brown', 'DishWasher', 1995-08-11, 'PM'),
+(027, 'Lebron James', 'Manager', 1984-12-30, 'AM'),
+(028, 'Kevin Durant', 'Manager', 1987-09-09, 'PM'),
+(029, 'Steph Curry' , 'Maitre', 1987-07-27, 'AM'),
+(030, 'James Harden' , 'Maitre', 1989-08-28, 'AM'),
+(031, 'Kawhi Leonard' , 'Maitre', 1990-09-14, 'AM'),
+(032, 'Damian Lillard' , 'Maitre', 1987-07-27, 'AM'),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
