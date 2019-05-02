b587e08370bb48e38a336e26e7de13a2a92718a4
RickJames7 <48032896+RickJames7@users.noreply.github.com>
2019-05-02 14:01:54 -0700

    Update FillTables.sql

--- src/FillTables.sql
+++ src/FillTables.sql
@@ -11,3 +11,20 @@ INSERT INTO Customer (CID, CName, Email) VALUES
 INSERT INTO HallOfFame (CID, InductionDate, itemName, photograph) VALUES
   (007, '1995-11-17', 'Chicken Chow Mein', 1),
   (006, '1997-08-25', 'Beef Egg Goo Young', 0);
+
+--data for employees
+INSERT INTO Employee (EID, name, jobTitle, DOB, shift) VALUES
+(001, Aarron Rodgers, HeadChef, 1988-03-12, AM),
+(002, Tom Brady, HeadChef, 1975-02-16, PM),
+(003, Jared Goff, SousChef, 1994-06-06, AM),
+(004, Phillip Rivers, SousChef, 1980-03-25, AM),
+(005, Jimmy Garropolo, SousChef, 1993-11-11, PM),
+(006, Patrick Mahomes, SousChef, 1995-05-11, PM),
+(007, Alex Smith, LineCook, 1987-07-13, AM),
+(008, Dereck Carr, LineCook, 1991-03-08, AM),
+(009, Kyler Murray, LineCook, 1997-01-28, AM),
+(010, Joe Montana, LineCook, 1965-11-01, AM),
+(011, Joe Theisman, LineCook, 1963-02-02, AM),
+(012, Baker Mayfield, LineCook, 1996-12-11, PM),
+(013, Drew Brees, LineCook, 1978-01-03, PM),
+(0, Drew Brees, LineCook, 1978-01-03, PM),
