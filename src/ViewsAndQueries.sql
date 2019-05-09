a72ea7bf81b57cdd03dea6a7068b7dfe516c0db2
Daniel Flores <Daniel.Flores03@student.csulb.edu>
2019-05-08 22:13:05 -0700

    Update ViewsAndQueries.sql

--- src/ViewsAndQueries.sql
+++ src/ViewsAndQueries.sql
@@ -1,3 +1,14 @@
+--17
+SELECT Employee.eName, Employee.EID, Employee.shift FROM Employee
+LEFT JOIN Chef ON Employee.EID = Chef.EID
+LEFT JOIN HeadChef ON Employee.EID = HeadChef.EID
+LEFT JOIN SousChef ON Employee.EID = SousChef.EID
+LEFT JOIN LineCook ON Employee.EID = LineCook.EID
+LEFT JOIN DishWasher ON Employee.EID = DishWasher.EID
+LEFT JOIN Manager ON Employee.EID = Manager.EID
+LEFT JOIN Maitre ON Employee.EID = Maitre.EID
+LEFT JOIN WaitStaff ON Employee.EID = WaitStaff.EID;
+
 --18
 --⅓ business query
 SELECT Customer.CID, Customer.CName, Customer.MaxSpiceLevel, Customer.Email, HallOfFame.InductionDate, HallOfFame.itemName FROM Customer
